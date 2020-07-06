6e7477481543c83732bf05ccbe16b7e67596162f
karl <xiezw@ruhrtec.cn>
2020-07-06 09:13:45 +0800

    spring boot quartz demo

---
+++ spring-boot-quartz/src/test/java/com/karl/springboot/quartz/SpringBootQuartzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.karl.springboot.quartz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootQuartzApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
